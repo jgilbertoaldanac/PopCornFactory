bb066322aa61bf4836ddd2edd981ec22222aff8a
Gilberto Aldana <jesus.aldana@potros.itson.edu.mx>
2021-10-09 16:41:40 -0700

    Initial commit

---
+++ app/src/main/java/aldana/gilberto/popcornfactory/MainActivity.kt
@@ -0,0 +1,21 @@
+package aldana.gilberto.popcornfactory
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.botonP)
+
+        button.setOnClickListener(){
+            var intent: Intent = Intent(this, catalogo::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/aldana/gilberto/popcornfactory/Pelicula.kt
@@ -0,0 +1,6 @@
+package aldana.gilberto.popcornfactory
+
+data class Pelicula (var titulo: String,
+                var image: Int,
+                var header: Int,
+                var sinopsis: String)
